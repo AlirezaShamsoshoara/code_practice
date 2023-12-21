0f2536cfb75fc2885a3e9264891ed0e2e84848f9
Alireza <a.shamsoshoara@gmail.com>
2023-12-20 18:03:07 -0800

    naive approach dna seq

--- 1_practice_learning/1_sliding_window/python/practice.py
+++ 1_practice_learning/1_sliding_window/python/practice.py
@@ -1,9 +1,36 @@
 
+def find_repeated_seq_naive(s, k):
+    """_summary_
 
+    Args:
+        s (_type_): _description_
+        k (_type_): _description_
 
-def find_repeated_seq(s, k):
+    Returns:
+        _type_: _description_
+    """
     n = len(s)
+    output_set = []
+    include_set = []
+    output_set = set()
     for i in range(0, n-k+1):
-        sub_str = s[i:i+k-1]
-        
-    return set()
+        sub_str = s[i:i+k]
+        if sub_str not in include_set:
+            include_set.append(sub_str)
+        else:
+            if isinstance(output_set, list):
+                if sub_str not in output_set:
+                    output_set.append(sub_str)
+            else:
+                output_set.add(sub_str)
+    return output_set
+
+
+if __name__ == "__main__":
+    STRING = "AAAAACCCCCAAAAACCCCCC"
+    k = 8
+
+    STRING = "GGGGGGGGGGGGGGGGGGGGGGGGG"
+    k = 9
+    output = find_repeated_seq_naive(s=STRING, k=k)
+    print(output)
