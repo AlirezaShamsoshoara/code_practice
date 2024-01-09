9b484f980a7dd6146f1134af1f2f094ef9987387
Alireza <a.shamsoshoara@gmail.com>
2024-01-08 22:12:09 -0800

    palindrome

--- 1_practice_learning/2_two_pointers/practice.py
+++ 1_practice_learning/2_two_pointers/practice.py
@@ -1,6 +1,32 @@
+def is_palindrome(s):
+    """_summary_
 
+    Args:
+        s (_type_): _description_
 
+    Returns:
+        _type_: _description_
+    """
+    # Replace this placeholder return statement with your code
+    left, right = 0, len(s) - 1
+
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
 
 
 if __name__ == "__main__":
-    pass
+    # ************************************************************
+    # *********************** DNA Sequence ***********************
+    test_cases = ["RACEACAR", "A", "ABCDEFGFEDCBA",
+                  "ABC", "ABCBA", "ABBA", "RACEACAR"]
+    i = 1
+    for s in test_cases:
+        print("Test Case #", i)
+        print(is_palindrome(s))
+        print("-" * 100, end="\n\n")
+        i = i + 1
+    # ************************************************************
